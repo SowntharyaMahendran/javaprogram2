021e4177cce143229239a8499aa523a737c5f7b0
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2019-02-19 05:05:11 -0800

    Add files via upload

---
+++ CommandlineInput.java
@@ -0,0 +1,12 @@
+
+public class CommandlineInput {
+	public static void main(String[] args){
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		int a= Integer.parseInt(args[0]);
+		int b= Integer.parseInt(args[1]);
+		int x=a+b;
+		System.out.println(x);
+	}
+
+}
