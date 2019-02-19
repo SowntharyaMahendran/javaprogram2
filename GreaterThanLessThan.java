f9a50cd5629cbc3f57703b4a54d745a3015e6c49
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2019-02-19 05:07:18 -0800

    Add files via upload

---
+++ GreaterThanLessThan.java
@@ -0,0 +1,13 @@
+
+public class GreaterThanLessThan {
+	public static void main(String[] args){
+		int counter=10;
+		int like=15;
+		boolean result1= counter < like;
+		boolean result2= counter > like;
+		System.out.println(" counter < like " + result1);
+		System.out.println(" counter > like " + result2);
+		
+	}
+
+}
