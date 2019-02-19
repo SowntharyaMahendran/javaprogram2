3cacdcd1de1a5c01b538829d0714a69dfe70c478
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2019-02-19 05:04:47 -0800

    Add files via upload

---
+++ arthimeticoperators.java
@@ -0,0 +1,36 @@
+
+public class arthimeticoperators {
+	public static void main(String[] args){
+		//arthimetic operators
+		int x=10;
+		int y=20;
+		int addResult=x+y;
+		int sub=x-y;
+		int mul=x*y;
+		int div=x/y;
+		int remain=x%y;
+		String firstName="sowntharya";
+		String lastName="Mahendran";
+		String fullName= firstName + lastName;
+		System.out.println("the resut is "+ addResult );
+		System.out.println("sub "+sub);
+		System.out.println("mul "+mul);
+		System.out.println("div "+div);
+		System.out.println("remain "+remain);
+		System.out.println("fullName "+fullName);
+		int like=1;
+		int totallike=++like;//like+1;
+		System.out.println("totallike "+totallike);
+		int counter=0;
+		counter+=1;//counter=counter+1;
+		System.out.println("counter is "+counter);
+		double price=19.24;
+		double gst=10;//10%;
+		float totalprice=((float)price)*10/100;
+		System.out.println("totalprice "+totalprice);
+		
+		
+		
+	}
+
+}
