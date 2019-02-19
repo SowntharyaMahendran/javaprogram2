b7fb36d0c85173b19ff32ed7e80974caca35178a
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2019-02-19 05:08:58 -0800

    Add files via upload

---
+++ types.java
@@ -0,0 +1,19 @@
+
+public class types {
+	public static void main(String[] args){
+		byte b=-120;
+		int i=70;
+		long l=9999;
+		short s=69;
+		float f=12.34f;
+		double d=555.67;
+		char c1=97;
+		System.out.println("byte"+b);
+		System.out.println("int"+i);
+		System.out.println("long"+l);
+		System.out.println("short"+s);
+		System.out.println("float"+f);
+		System.out.println("double"+d);
+		System.out.println("char"+c1);
+	}
+}
