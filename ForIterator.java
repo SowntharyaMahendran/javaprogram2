9457aaaf95231dd458ba4b3aaff61e2875c2ac3f
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2019-02-19 05:06:59 -0800

    Add files via upload

---
+++ ForIterator.java
@@ -0,0 +1,31 @@
+
+public class ForIterator {
+	public static void main(String[] args){
+		//print 0.5;
+		for(int i=0;i<=5;i++)
+		{
+		System.out.println(i);
+		}
+		//declaration
+		int counter;
+		for(counter=0;counter<=10;counter++)
+		{
+			System.out.println("counter");
+		}
+		int like=0;
+		while(like<=10){
+			System.out.println("like"+like);
+		like++;
+	}
+	
+		int dislike=1;
+		do{
+			System.out.println("dislike is: "+dislike);
+			dislike++;
+			
+		} while(dislike<11);
+	
+		}
+
+}
+
