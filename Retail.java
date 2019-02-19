1b644b97cb6628a3294e381745f873f7027a7ef1
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2019-02-19 05:08:21 -0800

    Add files via upload

---
+++ Retail.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Retail {
+public static void main(String[] args){
+	Scanner scan=new Scanner(System.in);
+	int.prodno=scan.nextInt();
+	System.out.println("enter the product number ");
+	int.quan=scan.nextInt();
+	
+	
+
+		
+	}
+
+	
+		
+	
+}
+}
