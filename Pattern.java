5fcbb6308917eb65ffb480b6be640c7ea9e2ca88
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2019-02-19 05:08:04 -0800

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,27 @@
+
+public class Pattern {
+	public static void main(String[] args){
+		int n=5,i,j;
+		for(i=1;i<=n;++i)
+		{
+			for(j=1;j<=i;++j)
+			{
+					System.out.print("*"+"");
+					
+				}
+			
+		System.out.println();
+	}
+		for(i=4;i<=n;--i)
+		{
+			for(j=1;j<=i;++j)
+			{
+					System.out.print("*"+"");
+					
+				}
+			
+		System.out.println();
+	}
+
+}
+}
