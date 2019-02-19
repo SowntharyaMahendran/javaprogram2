148d8bab123596167d80ddc8ecf652030842f9f3
SowntharyaMahendran <30342261+SowntharyaMahendran@users.noreply.github.com>
2019-02-19 05:06:17 -0800

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,17 @@
+
+public class Example1 {
+	public static void main(String[] args){
+int i;
+	for(i=0;i<=100;i++)
+		if(i==50){
+			System.out.println("i found a number");
+			break;
+		}
+		else
+		{
+			System.out.println("going on");
+		
+		}
+
+}
+}
